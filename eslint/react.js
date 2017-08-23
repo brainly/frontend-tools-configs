37ef17a36888b2f4c80379bae7cd119840c170b0
anmaciur <andrzej.maciurzynski@brainly.com>
2017-08-23 16:02:43 +0200

    React rules update

--- eslint/react.js
+++ eslint/react.js
@@ -12,14 +12,24 @@ module.exports = {
 
   'rules': {
     // REACT
+    // Enforces consistent naming for boolean props
+    'react/boolean-prop-naming': 'off',
+    // Prevent extraneous defaultProps on components
+    'react/default-props-match-prop-types': 'error',
     // Prevent missing displayName in a React component definition
     'react/display-name': 'error',
     // Forbid certain props on Components
     'react/forbid-component-props': 'off',
+    // Forbid certain elements
+    'react/forbid-elements': 'off',
     // Forbid certain propTypes
     'react/forbid-prop-types': 'off',
+    // Forbid foreign propTypes
+    'react/forbid-foreign-prop-types': 'error',
+    // Prevent using Array index in key props
+    'react/no-array-index-key': 'off',
     // Prevent passing children as props
-    'react/no-children-prop': 'off',
+    'react/no-children-prop': 'error',
     // Prevent usage of dangerous JSX properties
     'react/no-danger': 'off',
     // Prevent problem with children and props.dangerouslySetInnerHTML
@@ -27,7 +37,7 @@ module.exports = {
     // Prevent usage of deprecated methods
     'react/no-deprecated': 'error',
     // Prevent usage of setState in componentDidMount
-    'react/no-did-mount-set-state': 'off',
+    'react/no-did-mount-set-state': 'error',
     // Prevent usage of setState in componentDidUpdate
     'react/no-did-update-set-state': 'off',
     // Prevent direct mutation of this.state
@@ -38,10 +48,14 @@ module.exports = {
     'react/no-is-mounted': 'error',
     // Prevent multiple component definition per file
     'react/no-multi-comp': 'off',
+    // Prevent usage of shouldComponentUpdate when extending React.PureComponent
+    'react/no-redundant-should-component-update': 'error',
     // Prevent usage of the return value of React.render
     'react/no-render-return-value': 'error',
     // Prevent usage of setState
     'react/no-set-state': 'off',
+    // Prevent common casing typos
+    'react/no-typos': 'error',
     // Prevent using string references in ref attribute.
     'react/no-string-refs': 'off',
     // Prevent invalid characters from appearing in markup
@@ -49,15 +63,19 @@ module.exports = {
     // Prevent usage of unknown DOM property (fixable)
     'react/no-unknown-property': 'error',
     // Prevent definitions of unused prop types
-    'react/no-unused-prop-types': 'off',
+    'react/no-unused-prop-types': 'error',
+    // Prevent usage of setState in componentWillUpdate
+    'react/no-will-update-set-state': 'error',
     // Enforce ES5 or ES6 class for React Components
-    'react/prefer-es6-class': 'off',
+    'react/prefer-es6-class': 'error',
     // Enforce stateless React Components to be written as a pure function
     'react/prefer-stateless-function': 'off',
     // Prevent missing props validation in a React component definition
     'react/prop-types': 'error',
     // Prevent missing React when using JSX
     'react/react-in-jsx-scope': 'error',
+    // Enforce a defaultProps definition for every prop that is not a required prop
+    'react/require-default-props': 'off',
     // Enforce React components to have a shouldComponentUpdate method
     'react/require-optimization': 'off',
     // Enforce ES5 or ES6 class for returning value in render function
@@ -69,7 +87,9 @@ module.exports = {
     // Enforce propTypes declarations alphabetical sorting
     'react/sort-prop-types': 'off',
     // Enforce style prop value being an object
-    'react/style-prop-object': 'off',
+    'react/style-prop-object': 'error',
+    // Prevent void DOM elements (e.g. <img />, <br />) from receiving children
+    'react/void-dom-elements-no-children': 'error',
 
     // JSX
     // Enforce boolean attributes notation in JSX (fixable)
