46086971302a49ad9329232959d5b055d04b9d65
Andrzej Maciurzyński <andrzej.maciurzynski@brainly.com>
2019-01-30 10:09:00 +0100

    bump dependencies versions (#89)

--- packages/eslint-config-brainly/rules/babel.js
+++ packages/eslint-config-brainly/rules/babel.js
@@ -5,16 +5,32 @@ module.exports = {
     'new-cap': 'off',
     'babel/new-cap': 'error',
 
-    // require or disallow padding inside curly braces
-    'object-curly-spacing': 'off',
-    'babel/object-curly-spacing': ['error', 'never'],
+    // doesn't complain about optional chaining (var foo = bar?.a_b;`)
+    'camelcase': 'off',
+    'babel/camelcase': ['error', {'properties': 'never'}],
 
     // disallow this keywords outside of classes or class-like objects
     'no-invalid-this': 'off',
     'babel/no-invalid-this': 'error',
 
+    // require or disallow padding inside curly braces
+    'object-curly-spacing': 'off',
+    'babel/object-curly-spacing': ['error', 'never'],
+
+    // doesn't complain about JSX fragment shorthand syntax (<>foo</>;)
+    'quotes': 'off',
+    'babel/quotes': ['error', 'single'],
+
     // require or disallow use of semicolons instead of ASI
     'semi': 'off',
-    'babel/semi': 'error'
+    'babel/semi': 'error',
+
+    // doesn't fail when using do expressions or optional chaining (a?.b()).
+    'no-unused-expressions': 'off',
+    'babel/no-unused-expressions': 'off',
+
+    // doesn't complain when used with BigInt (typeof BigInt(9007199254740991) === 'bigint').
+    'valid-typeof': 'off',
+    'babel/valid-typeof': 'error'
   }
 };
--- packages/eslint-config-brainly/rules/flow.js
+++ packages/eslint-config-brainly/rules/flow.js
@@ -8,11 +8,19 @@ module.exports = {
     }
   },
   'rules': {
+    'flowtype/array-style-complex-type': [
+      'error',
+      'verbose'
+    ],
+    'flowtype/array-style-simple-type': [
+      'error',
+      'verbose'
+    ],
     'flowtype/boolean-style': [
       'error',
       'boolean'
     ],
-    'flowtype/define-flow-type': 'warn',
+    'flowtype/define-flow-type': 'error',
     'flowtype/delimiter-dangle': [
       'error',
       'never'
@@ -21,23 +29,35 @@ module.exports = {
       'error',
       'never'
     ],
+    'flowtype/newline-after-flow-annotation': [
+      'error',
+      'always'
+    ],
+    'flowtype/no-dupe-keys': 'error',
+    'flowtype/no-existential-type': 'off',
+    'flowtype/no-flow-fix-me-comments': 'off',
+    'flowtype/no-mutable-array': 'off',
     'flowtype/no-primitive-constructor-types': 'error',
     'flowtype/no-types-missing-file-annotation': 'error',
+    'flowtype/no-unused-expressions': 'off',
     // maybe enable in the future:
     'flowtype/no-weak-types': 'off',
     'flowtype/object-type-delimiter': [
       'error',
       'comma'
     ],
+    'flowtype/require-compound-type-alias': 'off',
+    'flowtype/require-exact-type': 'off',
     'flowtype/require-parameter-type': 'off',
-    'flowtype/require-return-type': [
-      'off'
-    ],
+    'flowtype/require-return-type': 'off',
+    'flowtype/require-types-at-top': 'off',
     'flowtype/require-valid-file-annotation': 'off',
+    'flowtype/require-variable-type': 'off',
     'flowtype/semi': [
       'error',
       'always'
     ],
+    'flowtype/sort-keys': 'off',
     'flowtype/space-after-type-colon': [
       'error',
       'always'
@@ -53,6 +73,10 @@ module.exports = {
     'flowtype/type-id-match': [
       'error',
       '^([A-Z][a-z0-9]*)+Type$'
+    ],
+    'flowtype/type-import-style': [
+      'error',
+      'declaration'
     ],
     'flowtype/union-intersection-spacing': [
       'error',
